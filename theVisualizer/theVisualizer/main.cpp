52a183069983e17aeb21fa6d73bca33eed1d4bf0
Marleen <marleen.traas@gmail.com>
2020-06-11 12:38:36 +0200

    First stage
    Added some comments

--- theVisualizer/theVisualizer/main.cpp
+++ theVisualizer/theVisualizer/main.cpp
@@ -6,14 +6,12 @@
 #define SCREEN_WIDTH 300
 #define SCREEN_HEIGHT 300
 
-GLfloat rotationX = 0.0f;
-GLfloat rotationY = 0.0f;
-
 int main(void)
 {
+	// Make a window
 	GLFWwindow *window;
 
-	// Initialize the library
+	// Initialize the library and make sure it works
 	if (!glfwInit())
 	{
 		return -1;
@@ -22,21 +20,28 @@ int main(void)
 	// Create a windowed mode window and its OpenGL context
 	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
 
+	// See if the window is actually created
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
+	// Make this windows context the current context
 	glfwMakeContextCurrent(window);
 
+	// Do as long as the window isn't closed
 	while (!glfwWindowShouldClose(window)) {
+		// Clear the screen at the beginning of every loop
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Swap the windows front and back buffers
 		glfwSwapBuffers(window);
+		// Look for events
 		glfwPollEvents();
 	}
 
+	// Terminate glfw at last
 	glfwTerminate();
 
 	return 0;
